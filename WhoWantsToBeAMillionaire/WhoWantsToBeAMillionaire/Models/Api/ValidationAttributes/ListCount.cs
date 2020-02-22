c88a7a249c4875a9ad887ec0f8878f079ddff9e6
Dan6erbond <moravrav@gmail.com>
2020-02-22 15:28:52 +0100

    create listcount validation attribute
    validation attribute that checks a list's size for min and max

---
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/Models/Api/ValidationAttributes/ListCount.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhoWantsToBeAMillionaire.Models.Api.ValidationAttributes
+{
+    public class ListCount : ValidationAttribute
+    {
+        public int MinCount { get; }
+        public int? MaxCount { get; set; }
+
+        public ListCount(int minCount, int? maxCount = null)
+        {
+            MinCount = minCount;
+            MaxCount = maxCount;
+        }
+
+        public string GetErrorMessage() => $"The list must contain between {MinCount} and {MaxCount} items.";
+
+        public override bool IsValid(object value)
+        {
+            var list = (List<object>) value;
+            var count = list.Count;
+
+            if (count < MinCount)
+            {
+                return false;
+            }
+
+            if (MaxCount != null && count > MaxCount)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
