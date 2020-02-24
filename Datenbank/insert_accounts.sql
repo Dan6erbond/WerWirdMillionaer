72387aeb0c0db1e50a0ae112867ad7263dc8bc2d
Dan6erbond <moravrav@gmail.com>
2020-02-24 15:38:55 +0100

    remove unnecessary files

--- Datenbank/insert_accounts.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `users` (`UserId`, `Username`, `IsAdmin`, `Salt`, `Password`) VALUES (NULL, 'TestAdmin', TRUE, '78LSgKSa5B1ERjOg+e/aRQ==', 'ZmfLY73R5Ufv15CBF6cCYslLnvZLVmwNlpndfrLlzeg=');
-INSERT INTO `users` (`UserId`, `Username`, `IsAdmin`, `Salt`, `Password`) VALUES (NULL, 'TestUser', FALSE, '78LSgKSa5B1ERjOg+e/aRQ==', 'ZmfLY73R5Ufv15CBF6cCYslLnvZLVmwNlpndfrLlzeg=');
-
