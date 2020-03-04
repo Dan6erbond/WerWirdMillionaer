a2e4e21b67955c3fb9cae8837c90ebf3e2c73a45
Dan6erbond <moravrav@gmail.com>
2020-03-04 11:04:36 +0100

    Setup GameState

--- WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/ClientApp/src/store/index.ts
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/ClientApp/src/store/index.ts
@@ -1,15 +1,18 @@
 import * as UserState from './Users';
+import * as GameState from './Games';
 
 // The top-level state object
 export interface ApplicationState {
     userState: UserState.UserState;
+    gameState: GameState.GameState;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    userState: UserState.reducer
+    userState: UserState.reducer,
+    gameState: GameState.reducer
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
