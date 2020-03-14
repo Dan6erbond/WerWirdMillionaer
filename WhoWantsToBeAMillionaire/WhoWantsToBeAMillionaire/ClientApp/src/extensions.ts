00b5653f06b57f54af0a6c3b2817f498912b953d
Dan6erbond <moravrav@gmail.com>
2020-03-14 07:25:38 +0100

    Create extensions file

---
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/ClientApp/src/extensions.ts
@@ -0,0 +1,15 @@
+﻿declare global {
+    interface Array<T> {
+        sortBy(property: string, ascending: boolean): Array<T>;
+    }
+}
+
+if (!Array.prototype.hasOwnProperty('sortBy')) {
+    Array.prototype.sortBy = function (property: string, ascending: boolean) {
+        return this.sort(function (a, b) {
+            return a[property] === b[property] ? 0 : (a[property] > b[property]) && ascending ? 1 : -1;
+        });
+    };
+}
+
+export {};
--- WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/ClientApp/src/store/Games.ts
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/ClientApp/src/store/Games.ts
@@ -3,18 +3,7 @@ import {AppThunkAction} from './';
 import {AnswerResult, ErrorResponse, QuizResult} from "./ApiResponse";
 import {AnswerSpecification, GameSpecification} from "./Specification";
 import {LeaderboardSort} from "../components/leaderboard/Leaderboard";
-
-declare global {
-    interface Array<T> {
-        sortBy(property: string, ascending: boolean): Array<T>;
-    }
-}
-
-Array.prototype.sortBy = function (property: string, ascending: boolean) {
-    return this.sort(function (a, b) {
-        return a[property] == b[property] ? 0 : (a[property] > b[property]) && ascending ? 1 : -1;
-    });
-};
+import '../extensions';
 
 export interface Category {
     categoryId: number;
