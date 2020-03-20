5b141891194aa49c00fa6f1d33c14e70c7ccbf27
Dan6erbond <moravrav@gmail.com>
2020-03-20 11:57:50 +0100

    Write firewall exception rules, code cleanup

--- WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/Startup.cs
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire/Startup.cs
@@ -62,7 +62,7 @@ namespace WhoWantsToBeAMillionaire
                 {
                     options.SaveToken = true;
                     options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuer = true,
                         ValidateAudience = true,
