6592860209eb65f38d494a2522df5c9f146c6258
Dan6erbond <moravrav@gmail.com>
2020-03-23 05:22:47 +0100

    Create login test method, update documentation with tests

--- WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.AutomatedUiTests/AutomatedUiTests.cs
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.AutomatedUiTests/AutomatedUiTests.cs
@@ -1,15 +1,20 @@
 using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace WhoWantsToBeAMillionaire.AutomatedUiTests
 {
     public class AutomatedUiTests : IDisposable
     {
         private readonly IWebDriver _driver;
-        public AutomatedUiTests()
+        private readonly ITestOutputHelper _outputHelper;
+        
+        public AutomatedUiTests(ITestOutputHelper outputHelper)
         {
+            _outputHelper = outputHelper;
             _driver = new ChromeDriver();
         }
         
@@ -23,9 +28,24 @@ namespace WhoWantsToBeAMillionaire.AutomatedUiTests
         public void TestSiteLoad()
         {
             _driver.Navigate().GoToUrl("https://localhost:44309");
-            
+
             Assert.Equal("WhoWantsToBeAMillionaire", _driver.Title);
             Assert.Contains("Log in", _driver.PageSource);
+        }
+
+        [Fact]
+        public void TestLogin()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44309");
+            
+            _driver.FindElement(By.Id("formBasicUsername")).SendKeys("TestUser");
+            _driver.FindElement(By.Id("formBasicPassword")).SendKeys("user123");
+            _driver.FindElement(By.CssSelector("#root > div > div > form > div:nth-child(3) > button")).Click();
+
+            Thread.Sleep(50);
+            
+            var url = _driver.Url;
+            Assert.Contains("quiz", url);
         }
     }
 }
---
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.AutomatedUiTests/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WhoWantsToBeAMillionaire.AutomatedUiTests
+{
+    public static class WebDriverExtensions
+    {
+        public static void WaitForComponents(this IWebDriver driver)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+        }
+    }
+}
