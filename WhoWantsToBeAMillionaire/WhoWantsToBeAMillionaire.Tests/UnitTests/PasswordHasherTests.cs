4fb0d62f034da34923e1a8925b934fd7270d57de
Dan6erbond <moravrav@gmail.com>
2020-03-24 10:40:35 +0100

    Create passwordhasher unit tests

---
+++ WhoWantsToBeAMillionaire/WhoWantsToBeAMillionaire.Tests/UnitTests/PasswordHasherTests.cs
@@ -0,0 +1,25 @@
+﻿using WhoWantsToBeAMillionaire.Models.Lifecycle.Users;
+using Xunit;
+
+namespace WhoWantsToBeAMillionaire.Tests.UnitTests
+{
+    public class PasswordHasherTests
+    {
+        [Fact]
+        public void TestHashPassword()
+        {
+            var password = "user123";
+            
+            var orgHasher = new PasswordHasher();
+
+            var salt = orgHasher.GenerateSalt().Salt;
+            
+            var orgResult = orgHasher.HashPassword(salt, password).Hashed;
+            
+            var newHasher = new PasswordHasher();
+            var newResult = newHasher.HashPassword(salt, password).Hashed;
+            
+            Assert.Equal(orgResult, newResult);
+        }
+    }
+}
